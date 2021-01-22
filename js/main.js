727124d2a2e8a394f5ba6f0276a1501096a21595
Cory <courtneynholley@gmail.com>
2021-01-22 16:50:26 -0600

    Feat: Random mole popup on pageload

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@
     let moles = new Array();
 
     $(document).ready(function() {
+
         let moles = new Array();
         $(".mole").each(function () {
             moles.push(this);
@@ -11,7 +12,27 @@
         console.log(moles);
 
         let moleToShow = moles[Math.floor(Math.random() * moles.length)];
-        console.log(moleToShow);
+
+        function selectRandomMole() {
+            console.log(moleToShow);
+            moleToShow.classList.remove("hideMole");
+
+            moleToShow.click(function() {
+                moleToShow.classList.add("hideMole");
+            });
+        }
+        selectRandomMole();
+
+
+
+        // function showRandomMole() {
+        //     moleToShow.classList.add("hideMole");
+        //     selectRandomMole();
+        // };
+        //
+        // moleToShow.onclick = function() {showRandomMole()};
+
+
 
     });
 
