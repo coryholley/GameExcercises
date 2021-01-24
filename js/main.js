29b655c7f7cc0cc8ec6bd56f7e8103abe32d4d1c
Cory <courtneynholley@gmail.com>
2021-01-24 14:19:27 -0600

    Feat: Added logic to mole game for different pop ups

--- js/main.js
+++ js/main.js
@@ -5,23 +5,41 @@
 
     $(document).ready(function() {
 
+        let score = 0;
+
         let moles = new Array();
-        $(".mole").each(function () {
-            moles.push(this);
-        })
+        moles.push($("#mole11"));
+        moles.push($("#mole12"));
+        moles.push($("#mole13"));
+        moles.push($("#mole21"));
+        moles.push($("#mole22"));
+        moles.push($("#mole23"));
+        moles.push($("#mole31"));
+        moles.push($("#mole32"));
+        moles.push($("#mole33"));
+
         console.log(moles);
 
         let moleToShow = moles[Math.floor(Math.random() * moles.length)];
 
-        function selectRandomMole() {
-            console.log(moleToShow);
-            moleToShow.classList.remove("hideMole");
+        console.log(moleToShow);
 
+        function selectRandomMole() {
+            let moleToShow = moles[Math.floor(Math.random() * moles.length)];
+            moleToShow.addClass("mole");
             moleToShow.click(function() {
-                moleToShow.classList.add("hideMole");
-            });
+                moleToShow.removeClass("mole");
+                score += 5;
+                console.log(score);
+                selectRandomMole();
+            })
         }
         selectRandomMole();
+
+        // if (moleToShow.click(function () {
+        //
+        // }));
+
 
 
 
