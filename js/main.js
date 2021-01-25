e1606a6f0447b0b168370cbb70fb0e333245c400
Cory <courtneynholley@gmail.com>
2021-01-24 18:11:50 -0600

    Feat: Logic practice, showing up on different tiles

--- js/main.js
+++ js/main.js
@@ -3,58 +3,100 @@
 
     let moles = new Array();
 
-    $(document).ready(function() {
+    $(document).ready(function () {
 
-        let score = 0;
+        let game = function () {
 
-        let moles = new Array();
-        moles.push($("#mole11"));
-        moles.push($("#mole12"));
-        moles.push($("#mole13"));
-        moles.push($("#mole21"));
-        moles.push($("#mole22"));
-        moles.push($("#mole23"));
-        moles.push($("#mole31"));
-        moles.push($("#mole32"));
-        moles.push($("#mole33"));
+            let score = 0;
+            let gameTime = 20 * 1000;
+            let turnTime = 1000;
+            let moleTiles = $("#gameContainer").find(".moleTile");
 
-        console.log(moles);
+            let moleInterval;
+            let moleLifeMin = 1000;
+            let moleLifeMax = 3 * 1000;
 
-        let moleToShow = moles[Math.floor(Math.random() * moles.length)];
+            let getRandomInt = function (min, max) {
+                return Math.floor(Math.random() * (max - min + 1)) + min;
+            }
 
-        console.log(moleToShow);
 
-        function selectRandomMole() {
-            let moleToShow = moles[Math.floor(Math.random() * moles.length)];
-            moleToShow.addClass("mole");
-            moleToShow.click(function() {
-                moleToShow.removeClass("mole");
+            moleTiles.on("click", ".moleTile", function () {
+                if (!this.hasClass("mole")) {
+                    return;
+                }
+                this.removeClass("mole");
                 score += 5;
-                console.log(score);
-                selectRandomMole();
-            })
-        }
-        selectRandomMole();
+                console.log(score)
+            });
 
-        // if (moleToShow.click(function () {
-        //
-        // }));
+            var startGame = function () {
+                score = 0;
 
+                // -------------------------
+                // Every turnTime, spawn a new mole.
+                // Record moleInterval so we can cancel it when the game ends.
+                // -------------------------
+                moleInterval = setInterval(spawnMole, turnTime);
+                // -------------------------
 
+                // -------------------------
+                // The game ends in gameTime
+                // -------------------------
+                setTimeout(endGame, gameTime);
+                // -------------------------
+            }
+            let endGame = function () {
+                // -------------------------
+                // Stop spawning new moles
+                // -------------------------
+                clearInterval(moleInterval);
+                // -------------------------
 
+                // -------------------------
+                // "hide" any existing moles.
+                // -------------------------
+                moleTiles.removeClass("mole");
+                // -------------------------
 
-        // function showRandomMole() {
-        //     moleToShow.classList.add("hideMole");
-        //     selectRandomMole();
-        // };
-        //
-        // moleToShow.onclick = function() {showRandomMole()};
+                alert("Game Over! Score: " + score);
+            }
+            let spawnMole = function (timeToLive) {
+                // -------------------------
+                // Select a random tile to set as a mole.
+                // You might adjust to only spawn moles where there are none already.
+                // -------------------------
+                let targetTile = $(moleTiles[getRandomInt(0, moleTiles.length - 1)]);
+                // -------------------------
 
+                // -------------------------
+                // Moles shall live for a random amount of time
+                // -------------------------
+                var timeToLive = getRandomInt(moleLifeMin, moleLifeMax);
+                // -------------------------
 
+                // -------------------------
+                // "show" the mole
+                // -------------------------
+                targetTile.addClass("mole");
+                // -------------------------
+
+                // -------------------------
+                // after timeToLive, automatically "hide" the mole
+                // -------------------------
+                setTimeout(function () {
+                    targetTile.removeClass("mole");
+                }, timeToLive);
+                // -------------------------
+            }
+            return {
+                startGame
+            };
+        }();
+
+        game.startGame();
 
     });
-
-
 
 
 })();
